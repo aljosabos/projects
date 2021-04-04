f60a43af9df58971c5f147f515efe3ce86215fab
aljosabos <45786154+aljosabos@users.noreply.github.com>
2021-04-04 19:23:36 +0200

    Add files via upload

---
+++ Projects/React/Youtube API/src/components/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+const KEY = "AIzaSyB78X4M0RS0JsiKH4MwYdVx9M9ngy-iK8Q";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("best videos");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar formSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
